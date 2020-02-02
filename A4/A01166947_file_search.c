af881c616f0e2f4ffd1399367d43813e9baea088
Cragzu <cragzu@gmail.com>
2020-02-02 11:56:55 -0800

    Created readLineOfFile function

--- A4/A01166947_file_search.c
+++ A4/A01166947_file_search.c
@@ -7,29 +7,62 @@
 
 #define MAX_LINE_SIZE 1000
 
-bool readFile(char *fileName, char content[MAX_LINE_SIZE]) {
+/*
+ * find a word in a line - function
+ * have another function that reads lines from file one at a time, calls the function
+ * if word isn't found, repeat til end of file
+ */
+
+void findWordInLine(char* word, char* line) {
+    char* beginningOfWord = word;
+    while (*line) {
+        if (word == line) {
+            word++;
+            if (!word) {
+                printf("Word found!");
+                return;
+            }
+        } else {
+            word = beginningOfWord;
+        }
+        line++;
+    }
+    return;
+}
+
+bool readLineOfFile(char *fileName, char content[MAX_LINE_SIZE], int lineNumber) {
     FILE *file = fopen(fileName, "r");
+    int lineCount = 0;
 
     if (file == NULL) {
         printf("Could not open file for reading.");
-        return false;
     }
 
     strcpy(content, "");
     char line[MAX_LINE_SIZE];
 
     while (fgets(line, MAX_LINE_SIZE, file) != NULL) {
-        strcat(content, line);
-        printf("%s", line);
+        lineCount++;
+        if (lineCount == lineNumber) {
+            strcat(content, line);
+            printf("in function: %s", line);
+            return content;
+        }
     }
 
     fclose(file);
     return true;
 }
 
-void searchForWordInFile(char fileName[], char wordToSearchFor[]) {
+void searchForWordInFile(char fileName[], char *wordToSearchFor) {
     char fileContent[MAX_LINE_SIZE];
 
-    readFile(fileName, fileContent);
+    readLineOfFile(fileName, fileContent, 2);
+
+
+
+    printf("%s", fileContent);
+
+    //readFile(fileName, fileContent, wordToSearchFor);
 
 }
