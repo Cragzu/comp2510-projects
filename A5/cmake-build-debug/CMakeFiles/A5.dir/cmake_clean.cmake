524e3a5d3ca28fed5126120533b8fa142eb5e066
Cragzu <cragzu@gmail.com>
2020-02-09 17:15:05 -0800

    Initial A5 commit

---
+++ A5/cmake-build-debug/CMakeFiles/A5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "A5.exe"
+  "A5.exe.manifest"
+  "A5.pdb"
+  "CMakeFiles/A5.dir/main.c.obj"
+  "libA5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
