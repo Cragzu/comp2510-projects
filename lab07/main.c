9e871178cefd3b614c8b24199892e83faffdafed
Cragzu <cragzu@gmail.com>
2020-02-28 11:44:11 -0800

    Read file and put data into one struct

--- lab07/main.c
+++ lab07/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define MAX_LINE_SIZE 1000
+#define INITIAL_ARRAY_SIZE 16
+#define MAX_NAME_SIZE 32
 
 struct Student {
     char *name;
     double gpa;
 };
 
-bool readFile(char *fileName, char content[MAX_LINE_SIZE]) {
+bool readFile(char *fileName) {
     FILE *file = fopen(fileName, "r");
 
     if (file == NULL) {
@@ -17,26 +19,28 @@ bool readFile(char *fileName, char content[MAX_LINE_SIZE]) {
         return false;
     }
 
-    strcpy(content, "");
-    char line[MAX_LINE_SIZE];
+    char readName[MAX_NAME_SIZE];
+    double readGPA;
+    fscanf(file, "%s %lf", readName, &readGPA);
 
-    while (fgets(line, MAX_LINE_SIZE, file) != NULL) {
-        strcat(content, line);
-        printf("%s", line);
-    }
+    struct Student studentInfo = {readName, readGPA};
+
+    printf("Name: %s", studentInfo.name);
+    printf("\nGPA: %lf", studentInfo.gpa);
 
     fclose(file);
     return true;
 }
 
 int main(int argc, char** argv) {
-    char content[MAX_LINE_SIZE];
+    struct Student* studentData = (struct Student *) malloc(INITIAL_ARRAY_SIZE * sizeof(struct Student));
 
-    struct Student chloe = {"Chloe", 4.0};
-    struct Student clint = {"Clint", 5.0};
-    printf("%s, %f\n", chloe.name, chloe.gpa);
-    printf("%s, %f", clint.name, clint.gpa);
+    readFile(argv[1]);
 
-    //readFile(argv[1], content);
+//    struct Student chloe = {"Chloe", 4.0};
+//    struct Student clint = {"Clint", 5.0};
+//    printf("%s, %f\n", chloe.name, chloe.gpa);
+//    printf("%s, %f", clint.name, clint.gpa);
+
     return 0;
 }
