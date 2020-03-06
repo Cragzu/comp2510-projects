defa29e3d7821859f42a1a6feb5db9e50952f163
Cragzu <cragzu@gmail.com>
2020-03-06 11:57:29 -0800

    Started char functionality

--- lab08/main.c
+++ lab08/main.c
@@ -17,11 +17,19 @@ bool intCompare(void *a, void *b) {
     return (firstValue > secondValue);
 }
 
-void genericBubbleSort(int *array, size_t arrSize, size_t datatypeSize) {
+// Pointer comparison of chars
+bool charCompare(void *a, void *b) {
+    char *firstValue = *(char **) a;
+    char *secondValue = *(char **) b;
+    return strcmp(firstValue, secondValue) > 0;
+}
+
+// Bubble sorts array of any data type
+void genericBubbleSort(void **array, size_t arrSize, size_t datatypeSize, bool (*compare)(void *, void *)) {
     while (true) {
         bool swapped = false;
         for (int i = 1; i < arrSize; i++) {
-            if (array[i - 1] > array[i]) {
+            if (compare(&array[i - 1], &array[i])) {
                 swapped = true;
                 swap(&array[i - 1], &array[i], datatypeSize);
             }
@@ -33,10 +41,19 @@ void genericBubbleSort(int *array, size_t arrSize, size_t datatypeSize) {
 }
 
 int main() {
-    int array[5] = {10, 1, -2, 5, 7};
-    genericBubbleSort(array, 5, sizeof(int));
+    // Test with ints
+    int intArray[5] = {10, 1, -2, 5, 7};
+    genericBubbleSort((void *) intArray, 5, sizeof(int), intCompare);
     for (int i = 0; i < 5; i++) {
-        printf("A[%d] = %d\n", i, array[i]);
+        printf("A[%d] = %d\n", i, intArray[i]);
     }
+
+    // Test with chars
+//    char charArray[5] = {'d', 'a', 'c', 'b', 'e'};
+//    genericBubbleSort((void *) charArray, 5, sizeof(char), charCompare);
+//    for (int i = 0; i < 5; i++) {
+//        printf("A[%d] = %c\n", i, charArray[i]);
+//    }
+
     return 0;
 }
