fc92661022ce58796ee4a2b06c6224272a058ca5
Cragzu <cragzu@gmail.com>
2020-03-20 09:43:13 -0700

    Created Link shortcut for node pointer

--- week11/main.c
+++ week11/main.c
@@ -1,28 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Node {
     int data;
     struct Node *next;
 };
 
+typedef struct Node* Link;
+
 void printLinkedList(struct Node *head) {
-    struct Node *current = head;
-    while (current != NULL) {
-        printf("%d", current->data);
-        current = current->next;
-        if (current != NULL) {
-            printf(" -> ");
-        }
+    if (!head) {
+        printf("\n");
+        return;
     }
-    printf("\n");
+    printf("%d", head->data);
+    if (head->next) {
+        printf(" -> ");
+    }
+
+    printLinkedList(head->next);
+}
+
+Link createNode(int data) {
+    Link link = (Link) malloc(sizeof(struct Node));
+    if (link == NULL) {
+        perror("memory issues!");
+        exit(1);
+    }
+    link->data = data;
+    link->next = NULL;
+    return link;
 }
 
 int main() {
-    struct Node node = {10, NULL};
-    struct Node node2 = {20, &node};
-    struct Node node3 = {4, &node2};
     // 4 -> 20 -> 10
+    Link node = createNode(10);
+    Link node2 = createNode(20);
+    Link node3 = createNode(4);
 
-    printLinkedList(&node3);
+    node3->next = node2;
+    node2->next = node;
+
+    printLinkedList(node3);
     return 0;
 }
