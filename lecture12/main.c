a2fdbcfa05431d9fc1e4071a9497f54d7ed00dc9
Cragzu <cragzu@gmail.com>
2020-03-26 17:03:28 -0700

    Created node constructor functions

--- lecture12/main.c
+++ lecture12/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Node {
     int data;
@@ -8,7 +9,51 @@ struct Node {
 
 typedef struct Node *TreeNode;
 
+TreeNode createTreeNode(int data) {
+    TreeNode treeNode = (TreeNode) malloc(sizeof(struct Node));
+    if (treeNode == NULL) {
+        perror("Memory issues!");
+        exit(1);
+    }
+    treeNode->data = data;
+    treeNode->left = NULL;
+    treeNode->right = NULL;
+    return treeNode;
+}
+
+TreeNode createTreeNodeWithChildren(int data, TreeNode left, TreeNode right) {
+    TreeNode treeNode = createTreeNode(data);
+    treeNode->left = left;
+    treeNode->right = right;
+    return treeNode;
+}
+
+
 int main() {
-    printf("Hello, World!\n");
+ /* Diagram of the tree...
+           5
+          / \
+        /     \
+       3       8
+      /\       /\
+     /  \     /  \
+    1    4   6    9
+*/
+
+    // function that takes the root of a tree and prints info
+
+    // bottom "leaves" have no children
+    TreeNode node_1 = createTreeNode(1);
+    TreeNode node_4 = createTreeNode(4);
+    TreeNode node_6 = createTreeNode(6);
+    TreeNode node_9 = createTreeNode(9);
+
+    // middle of the tree
+    TreeNode node_3 = createTreeNodeWithChildren(3, node_1, node_4);
+    TreeNode node_8 = createTreeNodeWithChildren(8, node_6, node_9);
+
+    // root
+    TreeNode root = createTreeNodeWithChildren(5, node_3, node_8);
+
     return 0;
 }
