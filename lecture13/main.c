bcc9e2b0d4b92fc28802c261edef2b487bbe4d18
Cragzu <cragzu@gmail.com>
2020-04-02 14:38:39 -0700

    Created push and pop functions

--- lecture13/main.c
+++ lecture13/main.c
@@ -35,18 +35,39 @@ void printLinkedList(DLink head) {
     printLinkedList(head->next);
 }
 
+void push(DLink *head, int data) {
+    DLink newHead = createNode(data, *head, NULL);
+    if (*head) {
+        (*head)->prev = newHead;
+    }
+    *head = newHead;
+}
+
+int pop(DLink *head) {
+    if (!*head) {
+        perror("Empty list passed to pop function!\n");
+        exit(1);
+    }
+
+    int data = (*head)->data;
+    DLink oldHead = *head;
+    *head = (*head)->next;
+
+    if (*head) {
+        (*head)->prev = NULL;
+    }
+    free(oldHead);
+    return data;
+}
+
 
 int main() {
-    DLink node_1 = createNode(1, NULL, NULL);
-    DLink node_2 = createNode(2, NULL, NULL);
-    DLink node_3 = createNode(3, NULL, NULL);
+    DLink head = NULL;
+    push(&head, 3);
+    push(&head, 2);
+    push(&head, 1);
 
-    node_1->next = node_2;
+    printLinkedList(head);
 
-    node_2->prev = node_1;
-    node_2->next = node_3;
-
-    node_3->prev = node_2;
-
-    printLinkedList(node_1);
+//    printf("%d", pop(&head));
 }
