2452d8cc1ef71c501e88ba2cd0e113a343bf503e
Cragzu <cragzu@gmail.com>
2020-04-16 13:38:12 -0700

    Initial commit for final exam

---
+++ final/cmake-build-debug/CMakeFiles/final.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/final.dir/main.c.obj"
+  "final.exe"
+  "final.exe.manifest"
+  "final.pdb"
+  "libfinal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
